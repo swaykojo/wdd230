82da6a21c1dc42523c9e47fc618fd8445e057226
swaykojo <awuahgodsway@gmail.com>
2023-06-30 17:35:45 +0000

    lesson10

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -18,16 +18,25 @@ const displayDirectory = (businesses) => {
         let busName = document.createElement("h2");
         let location = document.createElement("p");
         let contact = document.createElement("p");
+        let url = document.createElement("a");
+        let image = document.createElement("img");
         
-
+        // Build the element out to show the data.
         busName.textContent = `${business.name}`;
-        // Build the p content out to show the details
         location.textContent = `Address: ${business.address}`;
         contact.textContent = `Phone: ${business.phone}`;
+        url.textContent = `${business.link}`
 
-       
+
+        // Build the image portrait by setting all the relevant attribute
+        image.setAttribute("src", business.logo);
+        image.setAttribute("alt", `Logo for ${business.name}`);
+        image.setAttribute("loading", "lazy");
+        image.setAttribute("width", "100");
+        image.setAttribute("height", "100");
 
         //Append the section(card) with the created elements
+        card.appendChild(image);
         card.appendChild(busName);
         card.appendChild(location);
         card.appendChild(contact);
@@ -36,3 +45,21 @@ const displayDirectory = (businesses) => {
 
     });
 }
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
